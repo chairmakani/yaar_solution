5b5132ced7d2a423d141031f6ca64790b77bec2e
mdrahuman06 <ragu20140610@gmail.com>
2025-04-14 15:02:38 +0530

    files added

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vi6d0wuowjt4n&-dm*!f!x9s%0sytqjv(kghf!fp%lelgy028q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -12,7 +12,14 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(next_page='store:login'), name='logout'),
 ]
 
-# Serve media files during development
+# Serve media files even when DEBUG=False
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {
+        'document_root': settings.MEDIA_ROOT,
+    }),
+]
+
+# Serve static files during development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
